0c31371610e09e6c3a2dc6f549f7d9eb8d0521a0
Luis Fernando <luis.fernando@zeroaladerecha.com>
2023-12-12 17:22:36 -0600

    React helmet

--- src/components/patients/AddPatient.js
+++ src/components/patients/AddPatient.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { startAddPatient } from '../../actions/patients';
 import { removeMsgError, showMsgError, startLoading } from '../../actions/ui';
 import useForm from '../../hooks/useForm';
 import Sidebar from '../sidebar/Sidebar';
 import PatientsNavbar from './PatientsNavbar'
+import {Helmet} from "react-helmet";
 
 const AddPatient = () => {
 
@@ -36,107 +36,82 @@ const AddPatient = () => {
     dispatch(startLoading())
   }
 
-  
-  useEffect(() => {
-    // Actualizar las etiquetas meta
-    updateMetaTags();
-  }, []);
-
-  const updateMetaTags = () => {
-
-    console.log('location: ', window.location.hostname)
-
-    const existingOGMetaTags = document.querySelectorAll('meta[property^="og:"]');
-    existingOGMetaTags.forEach(tag => tag.remove());
-
-    createMetaTag("og:title", "Mi Pagina Web")
-    createMetaTag("og:type", "website")
-    createMetaTag("og:image", "https://www.webdevelopmentscripts.com/post-images/685b-change-browser-address-bar-color-chrome-android.jpeg", "image")
-    createMetaTag("og:url", "https://www.google.com/")
-    createMetaTag("og:description", "Descripción sobre mi y de la pagina creada")
-    createMetaTag("og:image:type", "image/jpeg")
-    // createMetaTag("og:image:width", "256")
-    // createMetaTag("og:image:height", "256")
-
-  }; 
-
-  const createMetaTag = (property, content, itemprop) => {
-    const metaTag = document.createElement("meta");
-    metaTag.setAttribute("property", property);
-    if(itemprop){
-      metaTag.setAttribute("itemprop", itemprop);
-    }
-    metaTag.setAttribute("content", content);
-    console.log('metaTag: ', metaTag)
-
-    document.head.appendChild(metaTag);
-  };
  
   return (
-    <div className="container-fluid p-0">
-      <div className="row m-0">
-        <div className="col-12 col-md-4 col-lg-3 p-0">
-          <Sidebar />
-        </div>
+    <div className="application">
+      <Helmet>
+        <meta property="og:title" content="Administrador de Pacientes" />
+        <meta property="og:description" content="Pagina para administrar tus pacientes, editar y eliminar registros" />
+        <meta property="og:image" content="https://www.webdevelopmentscripts.com/post-images/685b-change-browser-address-bar-color-chrome-android.jpeg" />
+        <meta property="og:url" content="https://patient-manager-41wnidiov-luispv02.vercel.app" />
+        <meta property="og:type" content="website" />
+      </Helmet>
+    
+      <div className="container-fluid p-0">
+        <div className="row m-0">
+          <div className="col-12 col-md-4 col-lg-3 p-0">
+            <Sidebar />
+          </div>
 
-        <div className="col-12 col-md-8 col-lg-9 p-0">
-          <div className="add__patient">
-            <PatientsNavbar />
+          <div className="col-12 col-md-8 col-lg-9 p-0">
+            <div className="add__patient">
+              <PatientsNavbar />
 
-            <div className="add__patient__content">
-              <h1>Add Patient</h1>
+              <div className="add__patient__content">
+                <h1>Add Patient</h1>
 
-              {
-                    msgError && <p className="msg-error">{msgError}</p>
-              }
+                {
+                      msgError && <p className="msg-error">{msgError}</p>
+                }
 
-              <form
-                onSubmit={handleAddPatient}
-              >
-                <input 
-                  type="text" 
-                  placeholder="Name" 
-                  name="name"
-                  onChange={handleInputChange}
-                  value={name}
-                />
+                <form
+                  onSubmit={handleAddPatient}
+                >
+                  <input 
+                    type="text" 
+                    placeholder="Name" 
+                    name="name"
+                    onChange={handleInputChange}
+                    value={name}
+                  />
 
-                <input 
-                  type="number" 
-                  placeholder="Telefono" 
-                  name="tel"
-                  onChange={handleInputChange}
-                  value={tel}
-                />
+                  <input 
+                    type="number" 
+                    placeholder="Telefono" 
+                    name="tel"
+                    onChange={handleInputChange}
+                    value={tel}
+                  />
 
-                <input 
-                  type="date" 
-                  name="date"
-                  onChange={handleInputChange}
-                  value={date}
-                />
+                  <input 
+                    type="date" 
+                    name="date"
+                    onChange={handleInputChange}
+                    value={date}
+                  />
 
-                <input 
-                  type="time" 
-                  name="time"
-                  onChange={handleInputChange}
-                  value={time}
-                />
+                  <input 
+                    type="time" 
+                    name="time"
+                    onChange={handleInputChange}
+                    value={time}
+                  />
 
-                <textarea 
-                  placeholder="Reason for the appointment" 
-                  name="reason"
-                  onChange={handleInputChange}
-                  value={reason}
-                ></textarea>
+                  <textarea 
+                    placeholder="Reason for the appointment" 
+                    name="reason"
+                    onChange={handleInputChange}
+                    value={reason}
+                  ></textarea>
 
-                <input 
-                  className={loading ? 'btn-add loading' :  'btn-add'}
-                  type="submit"
-                  value='Add patient'
-                  disabled={loading}
-                />
-              </form>
+                  <input 
+                    className={loading ? 'btn-add loading' :  'btn-add'}
+                    type="submit"
+                    value='Add patient'
+                    disabled={loading}
+                  />
+                </form>
+              </div>
             </div>
           </div>
         </div>
